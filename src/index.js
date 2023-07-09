27df6f555968ea0a7b7e6076a5692d2a9a9a3b06
26Yuliia <124077868+26Yuliia@users.noreply.github.com>
2023-07-09 20:01:40 +0300

    10

---
+++ src/index.js
@@ -0,0 +1,4 @@
+//import axios from 'axios';
+
+//axios.defaults.headers.common['x-api-key'] =
+//  'live_3bAFCqVANRjiTXZy3vcsKT6qUXFpjKifFDbA2af3bBoAOdP8f57GINIpMktnE16f';
---
+++ src/js/cat-api.js
@@ -0,0 +1,8 @@
+function getCats(fetchBreeds) {
+  const BASE_CATS_URL = 'https://api.thecatapi.com/v1/breeds';
+  const CAT_IMAGE_KEY = 'https://api.thecatapi.com/v1/images/search';
+  const API_KEY =
+    'live_3bAFCqVANRjiTXZy3vcsKT6qUXFpjKifFDbA2af3bBoAOdP8f57GINIpMktnE16f';
+
+  fetch(`${BASE_CATS_URL}/);
+}
